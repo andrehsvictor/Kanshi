eb9a9c59f35ad0db32fe7d2fd48ee620eff1cb13
André Victor <andrehsvictor@gmail.com>
2024-05-26 14:35:42 -0300

    feat: Add GradientOverlay component for displaying a gradient overlay

---
+++ src/pages/login/components/GradientOverlay.tsx
@@ -0,0 +1,5 @@
+export default function GradientOverlay() {
+  return (
+    <div className="absolute top-0 left-0 w-screen h-screen bg-gradient-to-b from-transparent via-transparent to-black"></div>
+  );
+}
