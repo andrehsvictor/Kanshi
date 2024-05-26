6cc9a43dc7cec3a982653fe5c1b71ebaadf2e8c0
André Victor <andrehsvictor@gmail.com>
2024-05-26 20:06:12 -0300

    feat: Add NotFoundPage component for handling 404 errors

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/login/LoginPage";
+import ErrorPage from "./pages/notFound/NotFoundPage";
 
 export default function Router() {
   return (
@@ -7,8 +8,10 @@ export default function Router() {
       <Routes>
         <Route path="/">
           <Route index element={<LoginPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="home" element={<h1>Home page</h1>} />
         </Route>
-        <Route path="*" element={<div>404 Not Found</div>} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   );
