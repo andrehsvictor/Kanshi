74680583737ca30365d5818d7ce4f414f3e944ed
André Victor <andrehsvictor@gmail.com>
2024-05-27 20:11:35 -0300

    feat: Add AnimePage component for displaying individual anime details

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/login";
 import ErrorPage from "./pages/notFound";
 import HomePage from "./pages/home";
+import AnimePage from "./pages/anime";
 
 export default function Router() {
   return (
@@ -11,6 +12,7 @@ export default function Router() {
           <Route index element={<LoginPage />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="home" element={<HomePage />} />
+          <Route path="anime/:id" element={<AnimePage />} />
         </Route>
         <Route path="*" element={<ErrorPage />} />
       </Routes>
