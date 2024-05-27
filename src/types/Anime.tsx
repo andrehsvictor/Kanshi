cc4656b7dd50bbe611a6a60b1ba93b6c72eae5da
André Victor <andrehsvictor@gmail.com>
2024-05-27 09:32:32 -0300

    feat: Add Anime type definition

---
+++ src/types/Anime.tsx
@@ -0,0 +1,10 @@
+export type Anime = {
+  id: number;
+  title: string;
+  description: string;
+  episodes: number;
+  seasons: number;
+  finished: boolean;
+  releaseDate: string;
+  imageUrl: string;
+};
